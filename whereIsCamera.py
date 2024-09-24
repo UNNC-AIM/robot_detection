a407bf982e1422f61a46b00249820e00c8601701
Calc1te <calcitevalentin@outlook.com>
2024-09-24 22:26:52 +0800

    never tested do not use

--- whereIsCamera.py
+++ whereIsCamera.py
@@ -24,3 +24,26 @@ def detect(frame):
         corners = tags[0].corners
         cv2.polylines(frame, [np.int32(corners)], True, (0, 255, 0), 5)
         return corners
+    
+while True:
+    ret1, frame1 = cam1.read()
+    ret2, frame2 = cam2.read()
+    if not ret1 or not ret2:
+        break
+    gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+    corners1 = detect(frame1)
+    gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+    corners2 = detect(frame2)
+    if corners1 and corners2:
+        ret1, rvec1, tvec1 = cv2.solvePnP(object_points, corners1, matrix, dist)
+        ret2, rvec2, tvec2 = cv2.solvePnP(object_points, corners2, matrix, dist)
+        if ret1 and ret2:
+            R1, _ = cv2.Rodrigues(rvec1)
+            R2, _ = cv2.Rodrigues(rvec2)
+            R = R2 @ R1.T
+            t = tvec2 - R @ tvec1
+            print('rotation: \n'R)
+            print(''t)
+
+    
+
